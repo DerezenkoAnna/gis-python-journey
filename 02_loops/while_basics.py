821119ccd2aa68a58352180cdf9335707b37fbe8
Anna Derezenko <derezenko.a.o@gmail.com>
2026-07-11 01:04:59 +0300

    Implement total accumulation using while loop

--- 02_loops/while_basics.py
+++ 02_loops/while_basics.py
@@ -1,10 +1,9 @@
 number = 1
 total = 0
 
-while number <=5:
-    
-    total = number + total
+while number <= 10:
 
+    total = number + total
     number = number + 1
 
-    print (total)
+print (total)
